4deb40186b6f38afd13779c805c9b5e726ada629
Zombermanic <101372930+Zombermanic@users.noreply.github.com>
2023-06-24 19:23:27 -0400

    Add files via upload

--- DjangoJardin/JardinVecino/urls.py
+++ DjangoJardin/JardinVecino/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from .views import inicio
+from . import views
 
 urlpatterns = [
-    path("inicio", inicio, name="inicio")
+    path("inicio", views.inicio, name="inicio"),
+    path("sobrenosotros.html", views.sobrenosotros, name="sobrenosotros"),
+    path("catalogo.html", views.catalogo, name="catalogo"),
+    path("historial.html", views.historial, name="historial"),
+    path("sub.html", views.sub, name="sub"),
+    path("admin.html", views.admin, name="admin"),
+    path("menu_sesion.html", views.menu_sesion, name="menu_sesion"),
+    path("seguimiento.html", views.seguimiento, name="seguimiento"),
+    path("sesionSocio.html", views.sesionSocio, name="sesionSocio"),
+    path("pedidos.html", views.pedidos, name="pedidos"),
+    path("PagAdmin.html", views.PagAdmin, name="PagAdmin"),
+    path("sesion.html", views.sesion, name="sesion"),
+    path("socio.html", views.socio, name="socio"),
 ]
--- DjangoJardin/JardinVecino/views.py
+++ DjangoJardin/JardinVecino/views.py
@@ -6,3 +6,50 @@ from .models import Usuario
 def inicio(request):
 
     return render(request, "inicio.html")
+
+def sobrenosotros(request):
+
+    return render(request, "sobrenosotros.html")
+
+def catalogo(request):
+
+    return render(request, "catalogo.html")
+
+def historial(request):
+
+    return render(request, "historial.html")
+
+def sub(request):
+
+    return render(request, "sub.html")
+def admin(request):
+
+    return render(request, "admin.html")
+
+def menu_sesion(request):
+
+    return render(request, "menu_sesion.html")
+
+def seguimiento(request):
+
+    return render(request, "seguimiento.html")
+
+def sesionSocio(request):
+
+    return render(request, "sesionSocio.html")
+
+def pedidos(request):
+
+    return render(request, "pedidos.html")
+
+def PagAdmin(request):
+
+    return render(request, "PagAdmin.html")
+
+def sesion(request):
+
+    return render(request, "sesion.html")
+
+def socio(request):
+
+    return render(request, "socio.html")
